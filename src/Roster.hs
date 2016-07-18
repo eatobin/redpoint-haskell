6a472bd355dc62abc9d2ce3813ebac1fc178855a
Eric Tobin <eatobin@gmail.com>
2016-07-17 21:57:13 -0500

    wow!

--- src/Roster.hs
+++ src/Roster.hs
@@ -2,9 +2,9 @@
 
 module Roster where
 
+import           Data.List.Utils
 import           Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
-import Data.List.Utils
 
 type PlrSym = String
 type Givee = PlrSym
@@ -26,62 +26,27 @@ data Player = Player
   , giftHist :: GiftHist
   } deriving (Show, Eq)
 
---["RinSta","Ringo Starr","JohLen","GeoHar"]
-
 makeGiftPair :: Givee -> Giver -> GiftPair
 makeGiftPair = GiftPair
 
 makePlayer :: Name -> GiftHist -> Player
 makePlayer = Player
 
--- makePlayerMap :: [String] -> Map PlrSym Player
--- makePlayerMap [s, n, ge, gr] =
---   Map.singleton s plr
---     where gp = makeGiftPair ge gr
---           plr = makePlayer n [gp]
+makePlayersList :: PlayersString -> PlayersList
+makePlayersList playersString = map (split ", ") playerString
+  where playerString = lines playersString
 
-makePlayerMap' :: [[String]] -> Map PlrSym Player
-makePlayerMap' [[s, n, ge, gr]] =
-  Map.fromList [(s, plr)]
+makePlayerKV :: [String] -> (PlrSym, Player)
+makePlayerKV [s, n, ge, gr] =
+  (s, plr)
     where gp = makeGiftPair ge gr
           plr = makePlayer n [gp]
-makePlayerMap' _ = Map.fromList [("RinSta",Player {name = "Ringo Starr", giftHist = [GiftPair {givee = "JohLen", giver = "GeoHar"}]})]
 
--- makePlayersList :: PlayersString -> PlayersList
--- makePlayersList playersString = map (split ", ") playerString
---   where playerString = lines playersString
---
--- makePlayersMap :: PlayersString -> [Map PlrSym Player]
--- makePlayersMap playersString = map makePlayerMap playersList
---   where playersList = makePlayersList playersString
+makePlayersKVList :: PlayersList -> [(PlrSym, Player)]
+makePlayersKVList = map makePlayerKV
 
--- makeMap = Map.singleton
--- makeRoster :: [(String, Player)] -> Map String Player
--- makeRoster = Map.fromList
+makePlayersMap :: [(PlrSym, Player)] -> Map PlrSym Player
+makePlayersMap  = Map.fromList
 
--- instance FromJSON Borrower where
---   parseJSON (Object v) = Borrower <$>
---                          v .: "name" <*>
---                          v .: "max-books"
---   parseJSON _ = error "Can't parse Borrower from YAML/JSON"
---
--- instance ToJSON Borrower where
---   toJSON (Borrower name maxBooks) = object ["name" .= name, "max-books" .= maxBooks]
---
--- makeBorrower :: Name -> MaxBooks -> Borrower
--- makeBorrower = Borrower
---
--- getName :: Borrower -> Name
--- getName Borrower {name} = name
---
--- setName :: Name -> Borrower -> Borrower
--- setName n br@Borrower {name} = br {name = n}
---
--- getMaxBooks :: Borrower -> MaxBooks
--- getMaxBooks Borrower {maxBooks} = maxBooks
---
--- setMaxBooks :: MaxBooks -> Borrower -> Borrower
--- setMaxBooks m br@Borrower {maxBooks} = br {maxBooks = m}
---
--- borrowerToString :: Borrower -> String
--- borrowerToString br = getName br ++ " (" ++ show (getMaxBooks br) ++ " books)"
+makeRoster :: PlayersString -> Map PlrSym Player
+makeRoster = makePlayersMap . makePlayersKVList . makePlayersList
