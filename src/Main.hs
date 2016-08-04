f4385cd628905dfb886077916c08d4646046b451
Eric Tobin <eatobin@gmail.com>
2016-08-04 15:09:24 -0500

    new main

--- src/Main.hs
+++ src/Main.hs
@@ -7,29 +7,35 @@ import Roster_Create
 
 import           Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
+import System.IO
 
-main = do
-    let m0 = Map.empty
-    let m1 = Map.insert "k1" 7 m0
-    let m  = Map.insert "k2" 13 m1
-    putStrLn $ "map: " ++ show m
+--main = do
+--    let m0 = Map.empty
+--    let m1 = Map.insert "k1" 7 m0
+--    let m  = Map.insert "k2" 13 m1
+--    putStrLn $ "map: " ++ show m
 
-    let v1 = m ! "k1"
-    putStrLn $ "v1: " ++ show v1
-    putStrLn $ "len: " ++ show (Map.size m)
+--    let v1 = m ! "k1"
+--    putStrLn $ "v1: " ++ show v1
+--    putStrLn $ "len: " ++ show (Map.size m)
 
-    let m' = Map.delete "k2" m
-    putStrLn $ "map: " ++ show m'
+--    let m' = Map.delete "k2" m
+--    putStrLn $ "map: " ++ show m'
 
-    let prs = Map.lookup "k2" m'
-    putStrLn $ "prs: " ++ show prs
+--    let prs = Map.lookup "k2" m'
+--    putStrLn $ "prs: " ++ show prs
 
-    let n = Map.fromList [("foo", 1), ("bar", 2)]
-    putStrLn $ "map: " ++ show n
+--    let n = Map.fromList [("foo", 1), ("bar", 2)]
+--    putStrLn $ "map: " ++ show n
 
-m9 = Map.empty
-m10 = Map.insert "k1" 77 m9
-m11 = Map.insert "k2" 133 m10
+--m9 = Map.empty
+--m10 = Map.insert "k1" 77 m9
+--m11 = Map.insert "k2" 133 m10
 
-myMap :: Ord k => [(k, a)] -> Map k a
-myMap = Map.fromList
+--myMap :: Ord k => [(k, a)] -> Map k a
+--myMap = Map.fromList
+
+main :: IO ()
+main = do 
+    contents <- readFile "beatles2014.txt"  
+    print (makeRosterList contents)
