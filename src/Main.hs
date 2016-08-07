227c7df0cc2f4295c6c966a72f6400f6e60e232f
Eric Tobin <eatobin@gmail.com>
2016-08-07 17:30:01 -0500

    printString progress

--- src/Main.hs
+++ src/Main.hs
@@ -35,6 +35,9 @@ import           System.IO
 
 main :: IO ()
 main = do
+    tvNoGivee <- atomically (newTVar [])
+    tvNoGiver <- atomically (newTVar [])
+    tvRosterPrintString <- atomically (newTVar "")
     rosterString <- readFile "beatles2014.txt"
     let rosterList = makeRosterList rosterString
     let rosterInfo = makeRosterInfo rosterList
@@ -60,45 +63,48 @@ main = do
     -- print (getGiverInRoster "PauMcc" roster 0)
     atomically $ modifyTVar tvRoster addYearInRoster
     roster <- readTVarIO tvRoster
+    noGivee <- readTVarIO tvNoGivee
+    noGiver <- readTVarIO tvNoGiver
+    rosterPrintString <- readTVarIO tvRosterPrintString
     print roster
 
 
 
 
-statusToString :: Books -> Borrowers -> String
-statusToString bksb brsb = "\n" ++
-  "--- Status Report of Test Library ---\n" ++
-  "\n" ++
-  libraryToString bksb brsb ++
-  "\n" ++
-  unlines (map bookToString bks) ++ "\n" ++
-  unlines (map borrowerToString brs) ++ "\n" ++
-  "--- End of Status Report ---" ++
-  "\n"
-    where bks = fst bksb
-          brs = fst brsb
+--statusToString :: Books -> Borrowers -> String
+--statusToString bksb brsb = "\n" ++
+--  "--- Status Report of Test Library ---\n" ++
+--  "\n" ++
+--  libraryToString bksb brsb ++
+--  "\n" ++
+--  unlines (map bookToString bks) ++ "\n" ++
+--  unlines (map borrowerToString brs) ++ "\n" ++
+--  "--- End of Status Report ---" ++
+--  "\n"
+--    where bks = fst bksb
+--          brs = fst brsb
   
   
-  (defn print-string-giving-roster [gift-year]
-  (let [no-givee (atom [])
-        no-giver (atom [])
-        roster-string (atom [])]
-    (swap! roster-string conj team-name " - Year " (+ first-year gift-year) " Gifts:\n\n")
-    (doseq [p (keys (into (sorted-map) (deref roster)))]
-      (let [player-name (get-player-name p)
-            givee-code (get-givee-code p gift-year)
-            giver-code (get-giver-code p gift-year)]
-        (if (= givee-code :none)
-          (swap! no-givee conj p)
-          (swap! roster-string conj player-name " is buying for " (get-player-name givee-code) "\n"))
-        (if (= giver-code :none)
-          (swap! no-giver conj p))))
-    (if-not (and (empty? (deref no-givee))
-                 (empty? (deref no-giver)))
-      (do
-        (swap! roster-string conj "\nThere is a logic error in this year's pairings.\nDo you see it?\nIf not... call me and I'll explain!\n\n")
-        (doseq [p (deref no-givee)]
-          (swap! roster-string conj (get-player-name p) " is buying for no one.\n"))
-        (doseq [p (deref no-giver)]
-          (swap! roster-string conj (get-player-name p) " is receiving from no one.\n"))))
-    (apply str (deref roster-string))))
+--  (defn print-string-giving-roster [gift-year]
+--  (let [no-givee (atom [])
+--        no-giver (atom [])
+--        roster-string (atom [])]
+--    (swap! roster-string conj team-name " - Year " (+ first-year gift-year) " Gifts:\n\n")
+--    (doseq [p (keys (into (sorted-map) (deref roster)))]
+--      (let [player-name (get-player-name p)
+--            givee-code (get-givee-code p gift-year)
+--            giver-code (get-giver-code p gift-year)]
+--        (if (= givee-code :none)
+--          (swap! no-givee conj p)
+--          (swap! roster-string conj player-name " is buying for " (get-player-name givee-code) "\n"))
+--        (if (= giver-code :none)
+--          (swap! no-giver conj p))))
+--    (if-not (and (empty? (deref no-givee))
+--                 (empty? (deref no-giver)))
+--      (do
+--        (swap! roster-string conj "\nThere is a logic error in this year's pairings.\nDo you see it?\nIf not... call me and I'll explain!\n\n")
+--        (doseq [p (deref no-givee)]
+--          (swap! roster-string conj (get-player-name p) " is buying for no one.\n"))
+--        (doseq [p (deref no-giver)]
+--          (swap! roster-string conj (get-player-name p) " is receiving from no one.\n"))))
+--    (apply str (deref roster-string))))
