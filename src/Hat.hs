52b5cff0a0ff17b8f3cdb713f72f5004cb243714
Eric Tobin <eatobin@gmail.com>
2016-08-12 07:36:49 -0500

    progress

--- src/Hat.hs
+++ src/Hat.hs
@@ -3,15 +3,20 @@ module Hat where
 import           Roster
 import           Roster_Utility
 import qualified Data.Map.Strict        as Map
-import Data.Random.Extras
+import Data.Random
 
 type Hat = [PlrSym]
 
 makeHat :: PlayersMap -> Hat
 makeHat pm = Map.keys pm
 
-drawPuck :: Hat -> PlrSym
-drawPuck h = 
-  if not (null h)
-    then sample 1 h
-    else "empty"
+--drawPuck :: Hat -> PlrSym
+--drawPuck h = 
+--  if not (null h)
+--    then runRVar (randomElement h)
+--    else "empty"
+reversed = randomElement [0,1,2]
+
+test = do
+  x <- reversed
+  return x
