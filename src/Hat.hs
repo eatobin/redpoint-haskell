743c8fa18e6583adf1e6db621f0af0066a9a330b
Eric Tobin <eatobin@gmail.com>
2016-08-14 09:47:37 -0500

    hat works without presence check

--- src/Hat.hs
+++ src/Hat.hs
@@ -12,16 +12,8 @@ makeHat = Map.keys
 
 drawPuck :: Hat -> IO PlrSym
 drawPuck h =
- if not (null h)
-   then fmap (h !!) (randomRIO (0, length h - 1))
-   else return "empty"
-
-half x = if even x
-  then Just (div x 2)
-  else Nothing
+ fmap (h !!) (randomRIO (0, length h - 1))
 
 removePuck :: PlrSym -> Hat -> Hat
 removePuck ps h =
-  if elem ps h
-    then filter (\p -> p /= ps) h
-    else h
+  filter (\p -> p /= ps) h
