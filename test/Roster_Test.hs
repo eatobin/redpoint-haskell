127685f18f9452bc95ea1a6758a48f6b34900041
Eric Tobin <eatobin@gmail.com>
2016-08-20 14:54:45 -0500

    works!

--- test/Roster_Test.hs
+++ test/Roster_Test.hs
@@ -7,9 +7,6 @@ import           Roster_Utility
 import           Test.HUnit
 
 plr1 = Player {pName = "Ringo Starr", giftHist = Seq.fromList [GiftPair {giver = "GeoHar", givee = "JohLen"}]}
--- plr2 = Player {pName = "John Lennon", giftHist = [GiftPair {giver = "RinSta", givee = "PauMcc"}]}
--- plr3 = Player {pName = "George Harrison", giftHist = [GiftPair {giver = "PauMcc", givee = "RinSta"}]}
--- plr4 = Player {pName = "Paul McCartney", giftHist = [GiftPair {giver = "JohLen", givee = "GeoHar"}]}
 roster = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]})
   ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]})
   ,("PauMcc",Player {pName = "Paul McCartney", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]})
@@ -22,36 +19,16 @@ rosterGR = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist =
   ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]})
   ,("PauMcc",Player {pName = "Paul McCartney", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]})
   ,("RinSta",Player {pName = "Ringo Starr", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})]
-testRosterString = "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen"
 rosterAddYear = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"},GiftPair {givee = "none", giver = "none"}]})
   ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"},GiftPair {givee = "none", giver = "none"}]})
   ,("PauMcc",Player {pName = "Paul McCartney", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"},GiftPair {givee = "none", giver = "none"}]})
   ,("RinSta",Player {pName = "Ringo Starr", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"},GiftPair {givee = "none", giver = "none"}]})]
-partialString = "The Partial Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, none, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, none\n"
 partialRoster = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]})
   ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "none", giver = "RinSta"}]})
   ,("PauMcc",Player {pName = "Paul McCartney", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "none"}]})
   ,("RinSta",Player {pName = "Ringo Starr", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})]
--- rstrLst = [("RinSta", Player {pName = "Ringo Starr", giftHist = [GiftPair {giver = "GeoHar", givee = "JohLen"}]}), ("JohLen", plr2), ("GeoHar", plr3), ("PauMcc", plr4)]
--- rstrMap = Map.fromList rstrLst
--- johLen = rstrMap ! "JohLen"
--- pauMcc = rstrMap ! "PauMcc"
-gh0 = Seq.fromList [GiftPair {giver = "GeoHar", givee = "JohLen"}]
-gh1 = Seq.fromList [GiftPair {giver = "GeoHarX", givee = "JohLenX"}, GiftPair {giver = "EriTob", givee = "ScoTob"}]
--- fstPr = gh1 !! 0
-gp1 = GiftPair {giver = "RinSta", givee = "RinSta"}
--- ["RinSta" "Ringo Starr" "JohLen" "GeoHar"]
-
-
-recipRoster = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "JohLen"}]})
-  ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "GeoHar"}]})]
-
-
--- testRosterString = "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen"
 testRosterList = [["The Beatles","2014"],["RinSta","Ringo Starr","JohLen","GeoHar"],["JohLen","John Lennon","PauMcc","RinSta"],["GeoHar","George Harrison","RinSta","PauMcc"],["PauMcc","Paul McCartney","GeoHar","JohLen"]]
 testRosterLine = ["The Beatles","2014"]
--- roster = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = [GiftPair {givee = "RinSta", giver = "PauMcc"}]})]
-makeNewGH = gh0 Seq.|> gp1
 
 testGetRosterName = (~=?)
   "The Beatles"
--- test/Rules_Test.hs
+++ test/Rules_Test.hs
@@ -1,11 +1,28 @@
 module Rules_Test where
 
+import qualified Data.Map.Strict as Map
+import qualified Data.Sequence   as Seq
 import           Hat
 import           Roster
 import           Roster_Test
 import           Roster_Utility
 import           Rules
 import           Test.HUnit
+
+recipRoster = Map.fromList [("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "JohLen"}]})
+  ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "GeoHar"}]})]
+rosterP0 = Map.fromList [("EriTob",Player {pName = "Eric Tobin", giftHist = Seq.fromList [GiftPair {givee = "KarLav", giver = "PauMcc"}]})
+  ,("GeoHar",Player {pName = "George Harrison", giftHist = Seq.fromList [GiftPair {givee = "PauMcc", giver = "JohLen"}]})
+  ,("JohLen",Player {pName = "John Lennon", giftHist = Seq.fromList [GiftPair {givee = "GeoHar", giver = "RinSta"}]})
+  ,("KarLav",Player {pName = "Karen Lavengood", giftHist = Seq.fromList [GiftPair {givee = "RinSta", giver = "EriTob"}]})
+  ,("PauMcc",Player {pName = "Paul McCartney", giftHist = Seq.fromList [GiftPair {givee = "EriTob", giver = "GeoHar"}]})
+  ,("RinSta",Player {pName = "Ringo Starr", giftHist = Seq.fromList [GiftPair {givee = "JohLen", giver = "KarLav"}]})]
+
+rosterP4 =
+  let
+    extended = addYearInRoster $ addYearInRoster $ addYearInRoster $ addYearInRoster rosterP0
+  in
+    setGiveeInRoster "RinSta" 4 "KarLav" $ setGiveeInRoster "RinSta" 3 "EriTob" $ setGiveeInRoster "RinSta" 2 "PauMcc" $ setGiveeInRoster "RinSta" 1 "GeoHar" extended
 
 testGiveeNotSelf = (~=?)
   True
@@ -19,7 +36,43 @@ testGiveeNotRecipFail = (~=?)
   False
   (giveeNotRecip "JohLen" "GeoHar" 0 recipRoster)
 
+testGiveeNotRepeatFail1 = (~=?)
+  False
+  (giveeNotRepeat "RinSta" "JohLen" 2 rosterP4)
+
+testGiveeNotRepeatFail2 = (~=?)
+  False
+  (giveeNotRepeat "RinSta" "GeoHar" 2 rosterP4)
+
+testGiveeNotRepeatPass3 = (~=?)
+  True
+  (giveeNotRepeat "RinSta" "KarLav" 2 rosterP4)
+
+testGiveeNotRepeatPass4 = (~=?)
+  True
+  (giveeNotRepeat "RinSta" "JohLen" 5 rosterP4)
+
+testGiveeNotRepeatPass5 = (~=?)
+  True
+  (giveeNotRepeat "RinSta" "GeoHar" 5 rosterP4)
+
+testGiveeNotRepeatFail6 = (~=?)
+  False
+  (giveeNotRepeat "RinSta" "PauMcc" 5 rosterP4)
+
+testGiveeNotRepeatFail7 = (~=?)
+  False
+  (giveeNotRepeat "RinSta" "EriTob" 5 rosterP4)
+
+testGiveeNotRepeatFail8 = (~=?)
+  False
+  (giveeNotRepeat "RinSta" "KarLav" 5 rosterP4)
+
 rulesTests = TestList [ testGiveeNotSelf, testGiveeNotRecipPass
-                      , testGiveeNotRecipFail ]
+                      , testGiveeNotRecipFail, testGiveeNotRepeatFail1
+                      , testGiveeNotRepeatFail2, testGiveeNotRepeatPass3
+                      , testGiveeNotRepeatPass4, testGiveeNotRepeatPass5
+                      , testGiveeNotRepeatFail6, testGiveeNotRepeatFail7
+                      , testGiveeNotRepeatFail8 ]
 
 runRulesTests = runTestTT $ TestList [ rulesTests ]
