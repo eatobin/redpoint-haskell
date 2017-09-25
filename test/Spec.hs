b9a0c3260b21834a0ddf3fd189f67d736e1943ca
Eric Tobin <eatobin@gmail.com>
2017-09-24 21:22:56 -0500

    testing progress

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS -Wall #-}
 
-import           Test.Hspec
-
+import qualified Data.Sequence  as Seq
 import           Roster_Utility
+import           Test.Hspec
 
 bs :: String
 bs = "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen"
@@ -12,6 +12,12 @@ rl = [["The Beatles","2014"],["RinSta","Ringo Starr","JohLen","GeoHar"],["JohLen
 
 pl :: RosterList
 pl = [["RinSta","Ringo Starr","JohLen","GeoHar"],["JohLen","John Lennon","PauMcc","RinSta"],["GeoHar","George Harrison","RinSta","PauMcc"],["PauMcc","Paul McCartney","GeoHar","JohLen"]]
+
+playerData :: RosterLine
+playerData = ["RinSta","Ringo Starr","JohLen","GeoHar"]
+
+thisPlayerKV :: PlayerKV
+thisPlayerKV = ("RinSta",Player {pName = "Ringo Starr", giftHist = Seq.fromList[GiftPair {givee = "JohLen", giver = "GeoHar"}]})
 
 main :: IO ()
 main = hspec $
@@ -24,3 +30,6 @@ main = hspec $
 
     it "testmakePlayersList" $
       makePlayersList rl `shouldBe` pl
+
+    it "testmakePlayerKV" $
+      makePlayerKV playerData `shouldBe` thisPlayerKV
