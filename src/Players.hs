896bfb404d6bdf53ddb5419ec0c186dcf7920f8a
Eric Tobin <eatobin@gmail.com>
2022-09-14 16:40:22 -0500

    in process

--- src/Players.hs
+++ src/Players.hs
@@ -27,5 +27,27 @@ playersGetPlayerName playerKey players
   where
     maybePlayer = Map.lookup playerKey players
 
+--playersAddYear :: Players -> Players
+--playersAddYear players =
+--  where
+--    []
+
 playersJsonStringToPlayers :: JsonString -> Maybe Players
 playersJsonStringToPlayers js = A.decodeStrict (BS.pack js) :: Maybe Players
+
+--λ> [(playerKey, newPlayer) | (playerKey, player) <- [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),
+--                  ("JohLen", Player {playerName = "John Lennon", giftHistory = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),
+--                  ("PauMcc", Player {playerName = "Paul McCartney", giftHistory = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]}),
+--                  ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})
+--                ]]
+-- [("GeoHar",Player {playerName = "George Harrison", giftHistory = fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),("JohLen",Player {playerName = "John Lennon", giftHistory = fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),("PauMcc",Player {playerName = "Paul McCartney", giftHistory = fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]}),("RinSta",Player {playerName = "Ringo Starr", giftHistory = fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})]
+
+--  def playersAddYear(players: Map[String, Player]): Map[String, Player] = {
+--    val newPlayers = for ((playerKey, player) <- players) yield {
+--      val gh = player.giftHistory
+--      val ngh = giftHistoryAddYear(playerKey, gh)
+--      val nplr = playerUpdateGiftHistory(ngh, player)
+--      playerKey -> nplr
+--    }
+--    newPlayers
+--  }
