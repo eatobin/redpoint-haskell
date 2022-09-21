0b28c49881b4f260230f2e540ff5240540b2b1cc
Eric Tobin <eatobin@gmail.com>
2022-09-21 13:09:52 -0500

    progress

--- src/Rules.hs
+++ src/Rules.hs
@@ -1,4 +1,4 @@
-module Rules (rulesGiveeNotSelf) where
+module Rules (rulesGiveeNotSelf, rulesGiveeNotReciprocal) where
 
 import Gift_History
 import Gift_Pair
@@ -10,11 +10,11 @@ rulesGiveeNotSelf :: SelfKey -> Givee -> Bool
 rulesGiveeNotSelf ps ge =
   ps /= ge
 
---rulesGiveeNotReciprocal :: SelfKey -> Givee -> GiftYear -> Players -> Bool
---rulesGiveeNotReciprocal selfKey givee giftYear players =
---  selfKey /= myReciprocal
---    where
---      myReciprocal = playersGetGivee(givee players giftYear)
+rulesGiveeNotReciprocal :: Givee -> Players -> GiftYear -> SelfKey -> Bool
+rulesGiveeNotReciprocal gee plrs giftYear selfKey =
+  selfKey /= myReciprocal
+  where
+    myReciprocal = playersGetGivee gee plrs giftYear
 
 --giveeNotRepeat :: PlrSym -> Givee -> GYear -> PlayersMap -> Bool
 --giveeNotRepeat ps ge gy pm =
