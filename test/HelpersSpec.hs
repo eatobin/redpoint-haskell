0896c0e52043f99ccedd3190df8e454e4f9c8efe
Eric Tobin <eatobin@gmail.com>
2022-10-25 17:31:30 -0500

    works!!!!

--- test/HelpersSpec.hs
+++ test/HelpersSpec.hs
@@ -27,7 +27,12 @@ spec = do
       borrowerToString br1
         `shouldBe` "Borrower1 (1 books)"
 
-  describe "helpersReadFileIntoJsonString" $ do
-    it "given a filepath, returns Either a JsonString or an ErrorString" $
+  describe "helpersReadFileIntoJsonString - PASS" $ do
+    it "given a valid filepath and JSON file, returns a Right JsonString" $
       helpersReadFileIntoJsonString "resources-test/beatles.json"
         `shouldReturn` Right "{\n  \"rosterName\": \"The Beatles\",\n  \"rosterYear\": 2014,\n  \"players\": {\n    \"PauMcc\": {\n      \"playerName\": \"Paul McCartney\",\n      \"giftHistory\": [\n        {\n          \"givee\": \"GeoHar\",\n          \"giver\": \"JohLen\"\n        }\n      ]\n    },\n    \"GeoHar\": {\n      \"playerName\": \"George Harrison\",\n      \"giftHistory\": [\n        {\n          \"givee\": \"RinSta\",\n          \"giver\": \"PauMcc\"\n        }\n      ]\n    },\n    \"JohLen\": {\n      \"playerName\": \"John Lennon\",\n      \"giftHistory\": [\n        {\n          \"givee\": \"PauMcc\",\n          \"giver\": \"RinSta\"\n        }\n      ]\n    },\n    \"RinSta\": {\n      \"playerName\": \"Ringo Starr\",\n      \"giftHistory\": [\n        {\n          \"givee\": \"JohLen\",\n          \"giver\": \"GeoHar\"\n        }\n      ]\n    }\n  }\n}\n"
+
+  describe "helpersReadFileIntoJsonString - FAIL" $ do
+    it "given an invalid filepath, returns a Left ErrorString" $
+      helpersReadFileIntoJsonString "resources-test/no-file.json"
+        `shouldReturn` Left "file read error."
