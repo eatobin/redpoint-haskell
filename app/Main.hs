db115771783c4f0ec9bd3d16b9ded8363700bafd
Eric Tobin <eatobin@gmail.com>
2022-10-25 17:59:02 -0500

    main progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,8 @@
 module Main (main) where
 
-import System.IO
+import Helpers
 
 main :: IO ()
---main = do
---  putStrLn "What's your name?"
---  hFlush stdout
---  name <- getLine
---  putStrLn ("Hello " ++ name)
-
-main = putStrLn "What's your name, honey?" >> hFlush stdout >> getLine >>= putStrLn . ("Hello " ++)
+main = do
+  result <- helpersReadFileIntoJsonString "resources-test/beatles.json"
+  print result
