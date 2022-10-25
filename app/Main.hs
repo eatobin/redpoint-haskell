7ffcf53d108b857f784fae88e174e0363560f6a0
Eric Tobin <eatobin@gmail.com>
2022-10-25 15:06:08 -0500

    even better!

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main (main) where
 
 main :: IO ()
-main = do { putStrLn "What's your name?" 
-          ; name <- getLine  
-          ; putStrLn ("Hello " ++ name) 
-          }
+-- main = do { putStrLn "What's your name?" 
+--           ; name <- getLine  
+--           ; putStrLn ("Hello " ++ name) 
+--           }
+
+main = putStrLn "What's your name, honey?" >> getLine >>= putStrLn . ("Hello " ++)
