fe81089dc716b8090fe4a94c631058c4e0b4f606
Eric Tobin <eatobin@gmail.com>
2022-10-31 18:25:24 -0500

    temp

--- test/HelpersSpec.hs
+++ test/HelpersSpec.hs
@@ -2,9 +2,12 @@ module HelpersSpec (spec) where
 
 import Helpers
 import Test.Hspec
+import qualified Control.Concurrent.STM as STM
+import qualified Data.Map.Strict as Map
 
 spec :: Spec
 spec = do
+--  tvPlayers <- STM.atomically (STM.newTVar Map.empty)
   describe "helpersReadFileIntoJsonString - PASS" $ do
     it "given a valid filepath, returns a Right JsonString" $
       helpersReadFileIntoJsonString "resources-test/bad-json.json"
@@ -14,3 +17,8 @@ spec = do
     it "given an invalid filepath, returns a Left ErrorString" $
       helpersReadFileIntoJsonString "resources-test/no-file.json"
         `shouldReturn` Left "file read error."
+
+  describe "helpersRosterOrQuit - PASS-1" $ do
+    it "given a valid filepath and TVarPlayers, returns an IO (RosterName, RosterYear)" $
+        helpersRosterOrQuit "resources-test/no-file.json" Map.empty
+          `shouldReturn` ("yes", 42)
