db5a71742d863a23296aba6c2de1147f078afbb9
Eric Tobin <eatobin@gmail.com>
2022-10-31 20:02:29 -0500

    working!

--- test/HelpersSpec.hs
+++ test/HelpersSpec.hs
@@ -1,13 +1,35 @@
 module HelpersSpec (spec) where
 
-import Helpers
-import Test.Hspec
 import qualified Control.Concurrent.STM as STM
 import qualified Data.Map.Strict as Map
+import qualified Data.Sequence as Seq
+import Gift_Pair
+import Helpers
+import Player
+import Players
+import Test.Hspec
+
+--fromAcctSTMTVarInt :: STM.STM (STM.TVar Int)
+--fromAcctSTMTVarInt = STM.newTVar (375 :: Int)
+
+--emptyPlayersSTMTVarInt :: STM.TVar Players
+emptyPlayersSTMTVarPlayers :: STM.STM (STM.TVar Players)
+--emptyPlayersSTMTVarInt :: IO (STM.TVar Players)
+emptyPlayersSTMTVarPlayers = STM.newTVar (emptyPlayers :: Players)
+
+--emptyPlayersTVarInt <- STM.atomically emptyPlayersSTMTVarInt
+
+players1 :: Players
+players1 =
+  Map.fromList
+    [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),
+      ("JohLen", Player {playerName = "John Lennon", giftHistory = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),
+      ("PauMcc", Player {playerName = "Paul McCartney", giftHistory = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]}),
+      ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})
+    ]
 
 spec :: Spec
 spec = do
---  tvPlayers <- STM.atomically (STM.newTVar Map.empty)
   describe "helpersReadFileIntoJsonString - PASS" $ do
     it "given a valid filepath, returns a Right JsonString" $
       helpersReadFileIntoJsonString "resources-test/bad-json.json"
@@ -19,6 +41,7 @@ spec = do
         `shouldReturn` Left "file read error."
 
   describe "helpersRosterOrQuit - PASS-1" $ do
-    it "given a valid filepath and TVarPlayers, returns an IO (RosterName, RosterYear)" $
-        helpersRosterOrQuit "resources-test/no-file.json" Map.empty
-          `shouldReturn` ("yes", 42)
+    it "given a valid filepath and TVarPlayers, returns an IO (RosterName, RosterYear)" $ do
+      emptyPlayersTVarInt <- STM.atomically emptyPlayersSTMTVarPlayers
+      helpersRosterOrQuit "resources-test/beatles.json" emptyPlayersTVarInt
+        `shouldReturn` ("The Beatles", 2014)
