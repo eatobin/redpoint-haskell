0433777e527a67a4d2e69535429765413ddfd49e
Eric Tobin <eatobin@gmail.com>
2022-11-05 22:13:12 -0500

    GiftPairSpec.hs progress

--- test/GiftPairSpec.hs
+++ test/GiftPairSpec.hs
@@ -12,22 +12,8 @@ giftPair = GiftPair {givee = "GeoHar", giver = "JohLen"}
 spec :: Spec
 spec = do
   describe "giftPairUpdateGivee" $ do
-    it " should update a givee" $ giftPairUpdateGivee "NewBee" giftPair `shouldBe` GiftPair "NewBee" "JohLen"
+    it "should update a givee" $ giftPairUpdateGivee "NewBee" giftPair `shouldBe` GiftPair "NewBee" "JohLen"
   describe "giftPairUpdateGiver" $ do
-    it " should update a giver" $ giftPairUpdateGiver "NewBee" giftPair `shouldBe` GiftPair "GeoHar" "NewBee"
-
---  describe "setName" $ do
---    it "sets a new Borower name" $ setName "Borrower1" (Borrower "Jack" 1) `shouldBe` br1
---  describe "getMaxBooks" $ do
---    it "returns the Borrower's maxBooks" $ getMaxBooks br1 `shouldBe` 1
---  describe "setMaxBooks" $ do
---    it "sets a new Borower maxBooks" $
---      setMaxBooks 11 br1
---        `shouldBe` Borrower
---          { name = "Borrower1",
---            maxBooks = 11
---          }
---  describe "borrowerToString" $ do
---    it "returns a Borrower as a sting" $
---      borrowerToString br1
---        `shouldBe` "Borrower1 (1 books)"
+    it "should update a giver" $ giftPairUpdateGiver "NewBee" giftPair `shouldBe` GiftPair "GeoHar" "NewBee"
+  describe "giftPairJsonStringToGiftPair" $ do
+    it "convert from JSON" $ giftPairJsonStringToGiftPair jsonString `shouldBe` Just (GiftPair "GeoHar" "JohLen")
