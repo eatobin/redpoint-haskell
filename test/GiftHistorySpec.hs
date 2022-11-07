141b7a2701862ed6cd09d79b3fa93240622420b6
Eric Tobin <eatobin@gmail.com>
2022-11-06 21:44:17 -0600

    gift history progress

--- test/GiftHistorySpec.hs
+++ test/GiftHistorySpec.hs
@@ -8,22 +8,22 @@ import Test.Hspec
 jsonString :: JsonString
 jsonString = "[{\"giver\":\"JohLen\",\"givee\":\"GeoHar\"}]"
 
-giftHistory1 :: GiftHistory
-giftHistory1 = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]
+giftHistory :: GiftHistory
+giftHistory = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]
 
-giftHistory2 :: GiftHistory
-giftHistory2 = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "Yippee", giver = "Yippee"}]
+-- giftHistory2 :: GiftHistory
+-- giftHistory2 = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "Yippee", giver = "Yippee"}]
 
-giftHistory3 :: GiftHistory
-giftHistory3 = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "Givee1", giver = "Giver1"}]
+-- giftHistory3 :: GiftHistory
+-- giftHistory3 = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "Givee1", giver = "Giver1"}]
 
 spec :: Spec
 spec = do
   describe "giftHistoryAddYear" $ do
-    it "should add a new year" $ giftHistoryAddYear "Yippee" giftHistory1 `shouldBe` giftHistory2
+    it "should add a new year" $ giftHistoryAddYear "NewBee" giftHistory `shouldBe` Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "NewBee", giver = "NewBee"}]
+  describe "giftHistoryUpdateGiftHistory" $ do
+    it "should return an updated giftHistory" $ giftHistoryUpdateGiftHistory 0 (GiftPair "me" "you") giftHistory `shouldBe` Seq.fromList [GiftPair {givee = "me", giver = "you"}]
 
---  describe "giftPairUpdateGiver" $ do
---    it "should update a giver" $ giftPairUpdateGiver "NewBee" giftPair `shouldBe` GiftPair "GeoHar" "NewBee"
 --  describe "giftPairJsonStringToGiftPair" $ do
 --    it "should convert from JSON" $ giftPairJsonStringToGiftPair jsonString `shouldBe` Just giftPair
 --  describe "giftPairGiftPairToJsonString" $ do
