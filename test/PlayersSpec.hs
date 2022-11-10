fb9e6feecfa27b3e5318d5fb6d6404e50ab37939
Eric Tobin <eatobin@gmail.com>
2022-11-10 17:27:45 -0600

    begin players

---
+++ test/PlayersSpec.hs
@@ -0,0 +1,23 @@
+module PlayersSpec (spec) where
+
+import qualified Data.Sequence as Seq
+import Gift_Pair
+import Player
+import Test.Hspec
+
+jsonString :: JsonString
+jsonString = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"giver\":\"JohLen\",\"givee\":\"GeoHar\"}]}"
+
+player :: Player
+player = Player {playerName = "Paul McCartney", giftHistory = Seq.fromList [GiftPair {giver = "JohLen", givee = "GeoHar"}]}
+
+spec :: Spec
+spec = do
+  describe "playerSUpdateGiftHistory" $ do
+    it "should return an updated giftHistory" $
+      playerUpdateGiftHistory (Seq.fromList [GiftPair {givee = "nope", giver = "yup"}]) player
+        `shouldBe` Player "Paul McCartney" (Seq.fromList [GiftPair "nope" "yup"])
+  describe "playerSJsonStringToPlayer" $ do
+    it "should convert from JSON" $ playerJsonStringToPlayer jsonString `shouldBe` Just player
+  describe "playerSPlayerToJsonString" $ do
+    it "should convert to JSON" $ playerPlayerToJsonString player `shouldBe` jsonString
