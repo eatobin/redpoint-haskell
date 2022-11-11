0214954b55fb4a37752f00a7dc70e46b75615789
Eric Tobin <eatobin@gmail.com>
2022-11-10 20:23:44 -0600

    move helpers

--- test/PlayersSpec.hs
+++ test/PlayersSpec.hs
@@ -19,7 +19,21 @@ players =
       ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Seq.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})
     ]
 
+newBeePlayers :: Players
+newBeePlayers =
+  Map.fromList
+    [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Seq.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),
+      ("JohLen", Player {playerName = "John Lennon", giftHistory = Seq.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),
+      ("PauMcc", Player {playerName = "Paul McCartney", giftHistory = Seq.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]}),
+      ("RinSta", Player {playerName = "New Bee", giftHistory = Seq.fromList [GiftPair {givee = "NewBee", giver = "NewBee"}]})
+    ]
+
 spec :: Spec
 spec = do
+  describe "playersUpdatePlayer" $ do
+      it "should return an updated player" $ playersUpdatePlayer "RinSta" Player {playerName = "New Bee", giftHistory = Seq.fromList [GiftPair {giver = "NewBee", givee = "NewBee"}]} players `shouldBe` newBeePlayers
+
+  
+  
   describe "playersJsonStringToPlayers" $ do
     it "should convert from JSON" $ playersJsonStringToPlayers jsonString `shouldBe` Just players
