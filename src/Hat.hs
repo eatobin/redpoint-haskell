29ff252ce40805e9d1616f5b51db3719dff76fd5
Eric Tobin <eatobin@gmail.com>
2023-01-11 20:29:36 -0700

    hat works

---
+++ src/Hat.hs
@@ -0,0 +1,27 @@
+module Hat (Hat, Discards, hatMakeHat, hatRemovePuck, hatDiscardGivee, hatReturnDiscards, hatJsonStringToHat) where
+
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Gift_Pair
+import Players
+
+type Hat = Set.Set PlayerKey
+
+type Discards = Set.Set PlayerKey
+
+hatMakeHat :: Players -> Hat
+hatMakeHat = Map.keysSet
+
+hatRemovePuck :: PlayerKey -> Hat -> Hat
+hatRemovePuck = Set.delete
+
+hatDiscardGivee :: Givee -> Discards -> Discards
+hatDiscardGivee = Set.insert
+
+hatReturnDiscards :: Discards -> Hat -> Hat
+hatReturnDiscards = Set.union
+
+hatJsonStringToHat :: JsonString -> Maybe Hat
+hatJsonStringToHat jsonString = A.decodeStrict (BS.pack jsonString) :: Maybe Hat
