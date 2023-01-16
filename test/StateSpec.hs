6e210eca9b229640138b424d7c08283ea11c395c
Eric Tobin <eatobin@gmail.com>
2023-01-15 17:54:13 -0700

    tests!!!

--- test/StateSpec.hs
+++ test/StateSpec.hs
@@ -1,49 +1,15 @@
 module StateSpec (spec) where
 
-import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import qualified Data.Vector as Vec
-import Gift_Pair
 import Hat
-import Player
-import Players
 import State
 import Test.Hspec
 
 testHat :: Hat
 testHat = Set.fromList ["RinSta"]
 
-stateSpecPlayers :: Players
-stateSpecPlayers =
-  Map.fromList
-    [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Vec.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),
-      ("JohLen", Player {playerName = "John Lennon", giftHistory = Vec.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),
-      ("PauMcc", Player {playerName = "Paul McCartney", giftHistory = Vec.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}]}),
-      ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Vec.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})
-    ]
-
--- TODO broken
-beatlesState :: State
-beatlesState =
-  State
-    { rosterName = "The Beatles",
-      rosterYear = 2014,
-      players = stateSpecPlayers,
-      giftYear = 0,
-      giveeHat = Set.empty,
-      giverHat = Set.empty,
-      maybeGivee = IO Nothing,
-      maybeGiver = IO Nothing,
-      discards = Set.empty,
-      quit = "n"
-    }
-
 spec :: Spec
 spec = do
   describe "stateDrawPuck" $ do
     it "should draw a puck" $ stateDrawPuck testHat `shouldReturn` Just "RinSta"
     it "should NOT draw a puck" $ stateDrawPuck Set.empty `shouldReturn` Nothing
-
---
---  describe "stateStartNewYear" $ do
---    it "start a new year" $ stateDrawPuck testHat `shouldReturn` Just "RinSta"
