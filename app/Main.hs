1614131e0fb7dc338fec6744c41c7d73eefd075e
Eric Tobin <eatobin@gmail.com>
2023-01-27 21:54:39 -0700

    main progress

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ module Main (RosterName, RosterYear, Quit, State (..), mainPrintResults, mainSel
 import qualified Control.Monad as CM
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Char as DC
 import qualified Data.Map.Strict as Map
 import qualified Data.Maybe as DM
 import qualified Data.Set as Set
@@ -205,10 +206,21 @@ mainJsonStringToState jsonString = A.decodeStrict (BS.pack jsonString) :: Maybe 
 hawksJson :: JsonString
 hawksJson = [r|{"rosterName":"Blackhawks","rosterYear":2010,"players":{"TroBro":{"playerName":"Troy Brouwer","giftHistory":[{"givee":"DavBol","giver":"JoeQue"}]},"PatKan":{"playerName":"Patrick Kane","giftHistory":[{"givee":"BryBic","giver":"CriHue"}]},"JoeQue":{"playerName":"Joel Quenneville","giftHistory":[{"givee":"TroBro","giver":"AndLad"}]},"NikHja":{"playerName":"Niklas Hjalmarsson","giftHistory":[{"givee":"BreSea","giver":"BriCam"}]},"TomKop":{"playerName":"Tomas Kopecky","giftHistory":[{"givee":"CriHue","giver":"DunKei"}]},"BryBic":{"playerName":"Bryan Bickell","giftHistory":[{"givee":"MarHos","giver":"PatKan"}]},"AntNie":{"playerName":"Antti Niemi","giftHistory":[{"givee":"JonToe","giver":"MarHos"}]},"PatSha":{"playerName":"Patrick Sharp","giftHistory":[{"givee":"BriCam","giver":"DavBol"}]},"DunKei":{"playerName":"Duncan Keith","giftHistory":[{"givee":"TomKop","giver":"AdaBur"}]},"BriCam":{"playerName":"Brian Campbell","giftHistory":[{"givee":"NikHja","giver":"PatSha"}]},"BreSea":{"playerName":"Brent Seabrook","giftHistory":[{"givee":"KriVer","giver":"NikHja"}]},"KriVer":{"playerName":"Kris Versteeg","giftHistory":[{"givee":"AndLad","giver":"BreSea"}]},"MarHos":{"playerName":"Marian Hossa","giftHistory":[{"givee":"AntNie","giver":"BryBic"}]},"AndLad":{"playerName":"Andrew Ladd","giftHistory":[{"givee":"JoeQue","giver":"KriVer"}]},"DavBol":{"playerName":"Dave Bolland","giftHistory":[{"givee":"PatSha","giver":"TroBro"}]},"CriHue":{"playerName":"Cristobal Huet","giftHistory":[{"givee":"PatKan","giver":"TomKop"}]},"JonToe":{"playerName":"Jonathan Toews","giftHistory":[{"givee":"AdaBur","giver":"AntNie"}]},"AdaBur":{"playerName":"Adam Burish","giftHistory":[{"givee":"DunKei","giver":"JonToe"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}|]
 
+mainLoop :: IO State -> IO ()
+mainLoop nextIOState = do
+  nextState <- nextIOState
+  if map DC.toLower (quit nextState) == "q"
+    then do
+      putStrLn "\nThis was fun!"
+      putStrLn "Talk about a position with Redpoint?"
+      putStrLn "Please call: Eric Tobin 773-679-6617"
+      putStrLn "Thanks! Bye...\n"
+    else putStrLn "Done!!"
+
 main :: IO ()
 main =
   do
     let maybeState :: Maybe State = mainJsonStringToState hawksJson
     case maybeState of
       Nothing -> putStrLn "So sorry, there is an error here."
-      Just state -> print state
+      Just firstState -> mainLoop (return firstState)
