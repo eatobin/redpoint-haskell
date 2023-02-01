08a3082523b7ffbf32be6d63c097a2e6c82d5433
Eric Tobin <eatobin@gmail.com>
2023-02-01 10:58:22 -0700

    fun person state code!!!

--- app/Main.hs
+++ app/Main.hs
@@ -15,13 +15,17 @@ regularPersonPush _ = (Pass, Locked)
 distractedPersonNoPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
 distractedPersonNoPush _ = (Wander, Unlocked)
 
+hastyPersonPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
+hastyPersonPush Locked = (Tut, Locked)
+hastyPersonPush Unlocked = (Pass, Locked)
+
 monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
 monday s0 =
-  let (a1, s1) = coin s0
+  let (a1, s1) = regularPersonPush s0
       (a2, s2) = distractedPersonNoPush s1
-      (a3, s3) = regularPersonPush s2
-      (a4, s4) = distractedPersonNoPush s3
-      (a5, s5) = regularPersonPush s4
+      (a3, s3) = hastyPersonPush s2
+      (a4, s4) = coin s3
+      (a5, s5) = hastyPersonPush s4
    in ([a1, a2, a3, a4, a5], s5)
 
 main :: IO ()
