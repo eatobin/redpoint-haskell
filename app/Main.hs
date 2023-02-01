05509bd2564c74cbfc2652685b504dcb09c46800
Eric Tobin <eatobin@gmail.com>
2023-02-01 09:14:47 -0700

    cleaned

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
-module Main (mainDrawPuck, main) where
-
-import qualified Data.Set as Set
-import Gift_Pair
-import Hat
-import qualified System.Random as Ran
-
-mainDrawPuck :: Hat -> IO (Maybe PlayerKey)
-mainDrawPuck hat
-  | Set.null hat = return Nothing
-  | otherwise = do
-    i :: Int <- Ran.randomRIO (0, Prelude.length hat - 1)
-    return (Just (Set.elemAt i hat))
+module Main (main) where
 
 main :: IO ()
 main =
