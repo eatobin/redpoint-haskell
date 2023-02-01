18e7d780e6a330793aa718cb53f07b21d9fd5101
Eric Tobin <eatobin@gmail.com>
2023-02-01 09:32:38 -0700

    first state code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main (main) where
 
+data TurnstileState = Locked | Unlocked
+  deriving (Eq, Show)
+
+data TurnstileOutput = Thank | Open | Tut
+  deriving (Eq, Show)
+
+coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
+coin _ = (Thank, Unlocked)
+
+push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+push Locked = (Tut, Locked)
+push Unlocked = (Open, Locked)
+
+monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+monday s0 =
+  let (a1, s1) = coin s0
+      (a2, s2) = push s1
+      (a3, s3) = push s2
+      (a4, s4) = coin s3
+      (a5, s5) = push s4
+   in ([a1, a2, a3, a4, a5], s5)
+
 main :: IO ()
 main =
   do
-    putStrLn "So sorry, there is an error here."
+    print (monday Locked)
