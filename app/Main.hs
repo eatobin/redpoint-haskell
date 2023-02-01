f702477688c935586402e8f918475203a6358cbc
Eric Tobin <eatobin@gmail.com>
2023-02-01 10:19:23 -0700

    distracted person state code

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 data TurnstileState = Locked | Unlocked
   deriving (Eq, Show)
 
-data TurnstileOutput = Thank | Open | Tut
+data TurnstileOutput = Thank | Open | Tut | Unchanged
   deriving (Eq, Show)
 
 coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
@@ -13,13 +13,17 @@ regularPersonPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
 regularPersonPush Locked = (Tut, Locked)
 regularPersonPush Unlocked = (Open, Locked)
 
+distractedPersonNoPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
+distractedPersonNoPush Locked = (Unchanged, Locked)
+distractedPersonNoPush Unlocked = (Unchanged, Unlocked)
+
 monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
 monday s0 =
   let (a1, s1) = coin s0
-      (a2, s2) = regularPersonPush s1
+      (a2, s2) = distractedPersonNoPush s1
       (a3, s3) = regularPersonPush s2
-      (a4, s4) = coin s3
-      (a5, s5) = regularPersonPush s4
+      (a4, s4) = distractedPersonNoPush s3
+      (a5, s5) = coin s4
    in ([a1, a2, a3, a4, a5], s5)
 
 main :: IO ()
