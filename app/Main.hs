b20d3de7886846a55d4bd4f1b730f47d1f2f3e95
Eric Tobin <eatobin@gmail.com>
2023-02-01 09:40:08 -0700

    regular person state code

--- app/Main.hs
+++ app/Main.hs
@@ -9,17 +9,17 @@ data TurnstileOutput = Thank | Open | Tut
 coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
 coin _ = (Thank, Unlocked)
 
-push :: TurnstileState -> (TurnstileOutput, TurnstileState)
-push Locked = (Tut, Locked)
-push Unlocked = (Open, Locked)
+regularPersonPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
+regularPersonPush Locked = (Tut, Locked)
+regularPersonPush Unlocked = (Open, Locked)
 
 monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
 monday s0 =
   let (a1, s1) = coin s0
-      (a2, s2) = push s1
-      (a3, s3) = push s2
+      (a2, s2) = regularPersonPush s1
+      (a3, s3) = regularPersonPush s2
       (a4, s4) = coin s3
-      (a5, s5) = push s4
+      (a5, s5) = regularPersonPush s4
    in ([a1, a2, a3, a4, a5], s5)
 
 main :: IO ()
