c37b31b25cdfa7287507a83481162aa9cf43cf6a
Eric Tobin <eatobin@gmail.com>
2023-02-02 10:01:01 -0700

    steps back

--- app/Main.hs
+++ app/Main.hs
@@ -3,42 +3,26 @@ module Main (main) where
 data TurnstileState = Locked | Unlocked
   deriving (Eq, Show)
 
-data TurnstileOutput = Thank | Pass | Tut | Wander
+data TurnstileOutput = Thank | Open | Tut
   deriving (Eq, Show)
 
 coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
 coin _ = (Thank, Unlocked)
 
-regularPersonPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
-regularPersonPush _ = (Pass, Locked)
-
-distractedPersonNoPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
-distractedPersonNoPush _ = (Wander, Unlocked)
-
-hastyPersonPush :: TurnstileState -> (TurnstileOutput, TurnstileState)
-hastyPersonPush Locked = (Tut, Locked)
-hastyPersonPush Unlocked = (Pass, Locked)
+push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+push Locked = (Tut, Locked)
+push Unlocked = (Open, Locked)
 
 monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
 monday s0 =
-  let (a1, s1) = regularPersonPush s0
-      (a2, s2) = distractedPersonNoPush s1
-      (a3, s3) = hastyPersonPush s2
+  let (a1, s1) = coin s0
+      (a2, s2) = push s1
+      (a3, s3) = push s2
       (a4, s4) = coin s3
-      (a5, s5) = hastyPersonPush s4
-   in ([a1, a2, a3, a4, a5], s5)
-
-tuesday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
-tuesday s0 =
-  let (a1, s1) = regularPersonPush s0
-      (a2, s2) = hastyPersonPush s1
-      (a3, s3) = distractedPersonNoPush s2
-      (a4, s4) = hastyPersonPush s3
-      (a5, s5) = hastyPersonPush s4
-   in ([a1, a2, a3, a4, a5], s5)
+      (a5, s5) = push s4
+    in ([a1, a2, a3, a4, a5], s5)
 
 main :: IO ()
 main =
   do
     print (monday Locked)
-    print (tuesday Locked)
