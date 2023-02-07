c9f954a53fd888574d8220f1647702972dfc61ec
Eric Tobin <eatobin@gmail.com>
2023-02-06 17:55:12 -0700

    MyState.hs testing

--- test/MyStateSpec.hs
+++ test/MyStateSpec.hs
@@ -13,8 +13,8 @@ import Test.Hspec
 testHat :: Hat
 testHat = Set.fromList ["RinSta"]
 
-players1 :: Players
-players1 =
+players0 :: Players
+players0 =
   Map.fromList
     [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Vec.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}]}),
       ("JohLen", Player {playerName = "John Lennon", giftHistory = Vec.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}]}),
@@ -22,12 +22,21 @@ players1 =
       ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Vec.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}]})
     ]
 
-beatlesState :: MyState
-beatlesState =
+players1 :: Players
+players1 =
+  Map.fromList
+    [ ("GeoHar", Player {playerName = "George Harrison", giftHistory = Vec.fromList [GiftPair {givee = "RinSta", giver = "PauMcc"}, GiftPair {givee = "GeoHar", giver = "GeoHar"}]}),
+      ("JohLen", Player {playerName = "John Lennon", giftHistory = Vec.fromList [GiftPair {givee = "PauMcc", giver = "RinSta"}, GiftPair {givee = "JohLen", giver = "JohLen"}]}),
+      ("PauMcc", Player {playerName = "Paul McCartney", giftHistory = Vec.fromList [GiftPair {givee = "GeoHar", giver = "JohLen"}, GiftPair {givee = "PauMcc", giver = "PauMcc"}]}),
+      ("RinSta", Player {playerName = "Ringo Starr", giftHistory = Vec.fromList [GiftPair {givee = "JohLen", giver = "GeoHar"}, GiftPair {givee = "RinSta", giver = "RinSta"}]})
+    ]
+
+beatlesState0 :: MyState
+beatlesState0 =
   MyState
     { rosterName = "The Beatles",
       rosterYear = 2014,
-      players = players1,
+      players = players0,
       giftYear = 0,
       giveeHat = Set.empty,
       giverHat = Set.empty,
@@ -37,9 +46,26 @@ beatlesState =
       quit = "n"
     }
 
+beatlesState1 :: MyState
+beatlesState1 =
+  MyState
+    { rosterName = "The Beatles",
+      rosterYear = 2014,
+      players = players1,
+      giftYear = 1,
+      giveeHat = Set.fromList ["GeoHar", "JohLen", "PauMcc", "RinSta"],
+      giverHat = Set.fromList ["GeoHar", "JohLen", "PauMcc", "RinSta"],
+      maybeGivee = Just "GeoHar",
+      maybeGiver = Just "PauMcc",
+      discards = Set.empty,
+      quit = "n"
+    }
+
 spec :: Spec
 spec = do
   describe "myStateDrawPuck" $ do
     it "should draw a puck from a hat" $ myStateDrawPuck testHat `shouldReturn` Just "RinSta"
     it "should NOT draw a puck from an empty hat" $ myStateDrawPuck Set.empty `shouldReturn` Nothing
-    it "should be equal" $ beatlesState `shouldBe` beatlesState
+
+  describe "myStateStartNewYear" $ do
+    it "should be equal" $ myStateStartNewYear (return beatlesState0) `shouldReturn` beatlesState1
