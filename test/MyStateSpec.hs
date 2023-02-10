8db9c58375aeeae4d65244df7b70b73b686889ea
Eric Tobin <eatobin@gmail.com>
2023-02-10 10:33:47 -0700

    myStateGiveeIsSuccess begin testing - tests!!!

--- test/MyStateSpec.hs
+++ test/MyStateSpec.hs
@@ -100,6 +100,4 @@ giveeIsSuccess = beforeAll (myStateStartNewYear (return beatlesState0)) $ do
         playersGetMyGivee goodGiver (players beatlesState2) (giftYear beatlesState2) `shouldBe` goodGivee
         playersGetMyGiver goodGivee (players beatlesState2) (giftYear beatlesState2) `shouldBe` goodGiver
         Set.notMember goodGivee (giveeHat beatlesState2) `shouldBe` True
---        Set.notMember goodGivee (giveeHat beatlesState2) `shouldBe` True
---        DM.fromJust (maybeGivee beatlesState2) `shouldNotBe` goodGivee
---        Set.member goodGivee (discards beatlesState2) `shouldBe` True
+        DM.isNothing (maybeGivee beatlesState2) `shouldBe` True
