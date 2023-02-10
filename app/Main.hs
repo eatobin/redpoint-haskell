5183c33da0bee3f7e0433edd41c0f96f8810edd8
Eric Tobin <eatobin@gmail.com>
2023-02-10 16:59:33 -0700

    still broken???

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,29 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main (main) where
 
-import Control.Monad.State
+import qualified Data.Char as DC
+import qualified Data.Maybe as DM
+import Gift_Pair
+import MyState
+import Text.RawString.QQ (r)
 
-data TurnstileState = Locked | Unlocked
-  deriving (Eq, Show)
+hawksJson :: JsonString
+hawksJson = [r|{"rosterName":"Blackhawks","rosterYear":2010,"players":{"TroBro":{"playerName":"Troy Brouwer","giftHistory":[{"givee":"DavBol","giver":"JoeQue"}]},"PatKan":{"playerName":"Patrick Kane","giftHistory":[{"givee":"BryBic","giver":"CriHue"}]},"JoeQue":{"playerName":"Joel Quenneville","giftHistory":[{"givee":"TroBro","giver":"AndLad"}]},"NikHja":{"playerName":"Niklas Hjalmarsson","giftHistory":[{"givee":"BreSea","giver":"BriCam"}]},"TomKop":{"playerName":"Tomas Kopecky","giftHistory":[{"givee":"CriHue","giver":"DunKei"}]},"BryBic":{"playerName":"Bryan Bickell","giftHistory":[{"givee":"MarHos","giver":"PatKan"}]},"AntNie":{"playerName":"Antti Niemi","giftHistory":[{"givee":"JonToe","giver":"MarHos"}]},"PatSha":{"playerName":"Patrick Sharp","giftHistory":[{"givee":"BriCam","giver":"DavBol"}]},"DunKei":{"playerName":"Duncan Keith","giftHistory":[{"givee":"TomKop","giver":"AdaBur"}]},"BriCam":{"playerName":"Brian Campbell","giftHistory":[{"givee":"NikHja","giver":"PatSha"}]},"BreSea":{"playerName":"Brent Seabrook","giftHistory":[{"givee":"KriVer","giver":"NikHja"}]},"KriVer":{"playerName":"Kris Versteeg","giftHistory":[{"givee":"AndLad","giver":"BreSea"}]},"MarHos":{"playerName":"Marian Hossa","giftHistory":[{"givee":"AntNie","giver":"BryBic"}]},"AndLad":{"playerName":"Andrew Ladd","giftHistory":[{"givee":"JoeQue","giver":"KriVer"}]},"DavBol":{"playerName":"Dave Bolland","giftHistory":[{"givee":"PatSha","giver":"TroBro"}]},"CriHue":{"playerName":"Cristobal Huet","giftHistory":[{"givee":"PatKan","giver":"TomKop"}]},"JonToe":{"playerName":"Jonathan Toews","giftHistory":[{"givee":"AdaBur","giver":"AntNie"}]},"AdaBur":{"playerName":"Adam Burish","giftHistory":[{"givee":"DunKei","giver":"JonToe"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}|]
 
-data TurnstileOutput = Thank | Open | Tut
-  deriving (Eq, Show)
-
-coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
-coin _ = (Thank, Unlocked)
-
-push :: TurnstileState -> (TurnstileOutput, TurnstileState)
-push Locked = (Tut, Locked)
-push Unlocked = (Open, Locked)
-
-coinS :: State TurnstileState TurnstileOutput
-coinS = state coin
-
-pushS :: State TurnstileState TurnstileOutput
-pushS = state push
-
-mondayS :: State TurnstileState [TurnstileOutput]
-mondayS = do
-  a1 <- coinS
-  a2 <- pushS
-  a3 <- pushS
-  a4 <- coinS
-  a5 <- pushS
-  return [a1, a2, a3, a4, a5]
+myMainLoop :: IO MyState -> IO ()
+myMainLoop nextStateIO = do
+  nextState <- nextStateIO
+  if map DC.toLower (quit nextState) == "q"
+    then do
+      putStrLn "\nThis was fun!"
+      putStrLn "Talk about a position with Redpoint?"
+      putStrLn "Please call: Eric Tobin 773-679-6617"
+      putStrLn "Thanks! Bye...\n"
+    else myMainLoop (myStateAskContinue (myStatePrintResults (myStateUpdateAndRunNewYear (return nextState))))
 
 main :: IO ()
 main =
   do
-    print (runState mondayS Locked)
+    let firstState = DM.fromJust (myStateJsonStringToMyState hawksJson)
+    myMainLoop (myStateAskContinue (myStatePrintResults (return firstState)))
