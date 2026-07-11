f0525f910ccbb0ec758e71061eed4e8a9cd3f3d8
Eric Tobin <eatobin@gmail.com>
2026-07-11 11:37:12 -0700

    reverse the run

--- src/DrunkenPirate.hs
+++ src/DrunkenPirate.hs
@@ -1,24 +1,24 @@
--- module DrunkenPirate (treasureMap) where
+module DrunkenPirate (treasureMap) where
 
--- import Control.Monad.Identity
+import Control.Monad.Identity (Identity (Identity, runIdentity))
 
--- stagger :: Int -> Identity Int
--- stagger p = Identity (p + 2)
+stagger :: Int -> Identity Int
+stagger p = Identity {runIdentity = p + 2}
 
--- crawl :: Int -> Identity Int
--- crawl p = Identity (p + 1)
-
--- -- treasureMap :: Int -> Identity Int
--- -- treasureMap pos =
--- --   stagger pos
--- --     >>= stagger
--- --     >>= crawl
+crawl :: Int -> Identity Int
+crawl p = Identity {runIdentity = p + 1}
 
 -- treasureMap :: Int -> Identity Int
 -- treasureMap pos =
---   crawl pos
+--   stagger pos
 --     >>= stagger
---     >>= stagger
+--     >>= crawl
+
+treasureMap :: Int -> Identity Int
+treasureMap pos =
+  crawl pos
+    >>= stagger
+    >>= stagger
 
 -- \$ stack repl --ghc-options -Wno-type-defaults
 -- λ> :l src/DrunkenPirate.hs
@@ -26,45 +26,45 @@
 -- treasureMap 100 -> Identity 105
 -- runIdentity (treasureMap 600) -> 605
 
-module DrunkenPirate (treasureMap, treasureMapX) where
+-- module DrunkenPirate (treasureMap, treasureMapX) where
 
-import Control.Monad.Identity (Identity (Identity, runIdentity))
+-- import Control.Monad.Identity (Identity (Identity, runIdentity))
 
-newtype Position t = Position t deriving (Show)
+-- newtype Position t = Position t deriving (Show)
 
-stagger :: (Num t) => Position t -> Position t
-stagger (Position d) = Position (d + 2)
+-- stagger :: (Num t) => Position t -> Position t
+-- stagger (Position d) = Position (d + 2)
 
-staggerX :: (Num t) => Identity t -> Identity t
-staggerX (Identity d) = Identity {runIdentity = d + 2}
+-- staggerX :: (Num t) => Identity t -> Identity t
+-- staggerX (Identity d) = Identity {runIdentity = d + 2}
 
-crawl :: (Num t) => Position t -> Position t
-crawl (Position d) = Position (d + 1)
+-- crawl :: (Num t) => Position t -> Position t
+-- crawl (Position d) = Position (d + 1)
 
-crawlX :: (Num t) => Identity t -> Identity t
-crawlX (Identity d) = Identity {runIdentity = d + 1}
+-- crawlX :: (Num t) => Identity t -> Identity t
+-- crawlX (Identity d) = Identity {runIdentity = d + 1}
 
-rtn :: p -> p
-rtn x = x
+-- rtn :: p -> p
+-- rtn x = x
 
-(>>==) :: t1 -> (t1 -> t2) -> t2
-x >>== f = f x
+-- (>>==) :: t1 -> (t1 -> t2) -> t2
+-- x >>== f = f x
 
-treasureMap :: (Num t) => Position t -> Position t
-treasureMap posM =
-  posM
-    >>== crawl
-    >>== stagger
-    >>== stagger
-    >>== rtn
+-- treasureMap :: (Num t) => Position t -> Position t
+-- treasureMap posM =
+--   posM
+--     >>== crawl
+--     >>== stagger
+--     >>== stagger
+--     >>== rtn
 
-treasureMapX :: Identity Int -> Identity Int
-treasureMapX posX =
-  posX
-    >>== crawlX
-    >>== staggerX
-    >>== staggerX
-    >>== rtn
+-- treasureMapX :: Identity Int -> Identity Int
+-- treasureMapX posX =
+--   posX
+--     >>== crawlX
+--     >>== staggerX
+--     >>== staggerX
+--     >>== rtn
 
 -- treasureMap (Position 100) -> Position 105
 -- treasureMap (Position (100 :: Int))
